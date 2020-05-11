537fa30737b393bb4ed1b5841a140232a96c34f7
ThiagoF435 <thiagoferreira435@gmail.com>
2020-05-11 11:29:27 -0300

    textToSpeech and SpeechToText

--- public/js/ibmWatson.js
+++ public/js/ibmWatson.js
@@ -1,9 +1,12 @@
 // Variavel para controlar o contexto do Dialogo
 var contextDialog = '{}';
 
-function sendMessageToAssistant(){
+function sendMessageToAssistant(textMessage){
+	//se nao recebeu a mensagem por parametro
 	//recupera mensagem digitada pelo usuario
-	var textMessage = document.chatForm.textMessage.value;
+	if(textMessage === undefined || textMessage === '')
+		var textMessage = document.chatForm.textMessage.value;
+	
 	chat = document.getElementById('chat');
 	
 	//na primeira chamada (boas vindas) textMessage é undefined
@@ -28,6 +31,11 @@ function sendMessageToAssistant(){
 					//exibe retorno da API e recupera o contexto para o proximo dialogo
 					chat.innerHTML += 'Chatbot -->' + returnedData.data.result.output.text + '<br>';
 					contextDialog = JSON.stringify(returnedData.data.result.context);
+					
+					//se o navegador nao for o chrome ou tiver habilitado o som da pagina
+					//chama o servico Text to Speech, passando o retorno do assistant
+					if(navigator.userAgent.indexOf("Chrome") === -1 || $('#muteButton').attr('class').match('off'))
+						sendTextToSpeech(JSON.stringify(returnedData.data.result.output.text));
 				}
 			}
 	)
@@ -50,5 +58,63 @@ $(document).keypress(
 //apos carregar todos os recursos da pagina, faz post para o servico
 //para exibir mensagem de boas vindas do chat
 $(document).ready(function(){
+	//se o navegador for o chrome pede ao usuario a permissao
+	if(navigator.userAgent.indexOf("Chrome") != -1){
+		document.getElementById('muteButton').setAttribute('style', 'font-size:40px;');
+		alert('Ative o som se quiser iniciar o dialogo de voz!');
+	}
+	
 	sendMessageToAssistant();
 });
+
+//permite a execucao do som no chrome
+function allowAutoPlay(){
+	$('#muteButton').attr('class', 'fas fa-volume-off');
+}
+
+//envia o texto para o TextToSpeech
+function sendTextToSpeech(textMessage){
+	//cria um elemento html de audio para tocar o audio retornado pela APi
+	var audioElement = document.createElement('audio');
+	//especifica o atributo type do audio
+	audioElement.setAttribute('type', 'audio/ogg;codecs=opus');
+	//define como source do audio o retorno do servico textToSpeech
+	audioElement.setAttribute('src', '/ibmWatson/textToSpeech?text=' + textMessage);
+	//toca o audio retornado
+	audioElement.play();
+}
+
+//envia audio do usuario e converte para texto
+function sendAudioToSpeechToText(blob) {
+	//criar um formulario para enviar o arquivo de audio
+	var form = new FormData();
+	form.append('audioFile', blob);
+	//post para o servico watsonSpeechToText
+	$.ajax({
+		url: 'ibmWatson/speechToText',
+		type: 'post',
+		data: form,
+		processData: false,
+		contentType: false,
+		//tratamento de erro de post
+		error: function (returnedData) {
+			alert ('Erro: ' + returnedData.status + ' ' + returnedData.statusText);
+		},
+		//tratamento de sucesso de processamento do post
+		success: function (returnedData){
+			//se ocorreu algum erro de processamento da Api
+			if(returnedData.status === 'ERRO')
+				alert('Erro: ' + returnedData.data);
+			//caso os dados tenham retornado com sucesso
+			else{
+				//recupera a conversao do audio do texto
+				if(returnedData.data.result.results[0] != undefined) {
+					var retorno = JSON.stringify(returnedData.data.result.results[0].
+					alternatives[0].transcript).replace(/"/g,'');
+					//envia o texto do audio para obter o retorno do chat
+					sendMessageToAssistant(retorno);
+				}
+			}
+		}
+	});
+}
--- routes/ibmWatson.js
+++ routes/ibmWatson.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 //recupera configuracoes de acesso aos servicos IBM Watson
 const ibmWatson = require('../lib/ibmWatsonCredentials');
+//adiciona package multer que permite recuperar stream de audio
+//enviado para o servido text to speech
+var multer = require('multer');
+var upload = multer();
 
 //post para o servico: IBM Watson Assistant
 router.post('/assistant', function(req,res,next){
@@ -25,6 +29,58 @@ router.post('/assistant', function(req,res,next){
 			res.json({ status: 'OK', data: response });
 	}
 	);
+});
+
+//---------------------------------------------------------------------------------
+// get para o servico: IBM Watson Text to Speech
+//---------------------------------------------------------------------------------
+router.get('/textToSpeech', async(req, res, next) => {
+	try{
+		//constroi json para o envio dos dados ao servico
+		var params = {
+			text: req.query.text,
+			accept: 'audio/mp3',
+			voice: 'pt-BR_IsabelaVoice'
+		};
+		//envia os dados ao servico e armazena o retorno no objeto result
+		const { result } = await ibmWatson.textToSpeech.synthesize(params);
+		result.on('response',(response) => {
+			//disponibiliza audio para o front-end
+			response.headers['content-disposition'] = `attachment;
+			filename=transcript.audio%2Fmp3`;
+		});
+		result.on('error', next);
+		result.pipe(res);
+	} catch(error){
+		res.send(error);
+	}
+});
+
+//--------------------------------------------------------
+//post pra o servico IBM Watson Speech to Text
+//--------------------------------------------------------
+router.post('/speechToText', upload.single('audioFile'), function(req, res, next) {
+	//recupera stream de audio para enviar ao servico
+	var audioStream = req.file;
+	
+	//constroi json para envio dos dados ao serviço
+	var params = {
+		audio: audioStream.buffer,
+		contentType: 'audio/l16; rate=44100',
+		interim_results: true,
+		model: 'pt-BR_BroadbandModel'
+	};
+	console.log(params);
+	
+	//envia os dados ao serviço e retorna o audio transcrito
+	ibmWatson.speechToText.recognize(params, function(error, response){
+		if(error)
+			res.json({status: 'ERRO', data: error.code + ' - ' + error.toString() });
+		else {
+			console.log(JSON.stringify(response.result, null, 2));
+			res.json({status: 'OK', data: response});
+		}
+	});
 });
 
 module.exports = router;
